8c574d6f253ff9bffdb96b32da5ac890452abb20
David Babbitt <davidb@spiceworks.com>
2015-04-21 01:38:37 -0500

    hook up with the right oauth2 client id

--- source/javascripts/downloads.js
+++ source/javascripts/downloads.js
@@ -2,15 +2,15 @@
   'use strict';
 
   developers.downloads = {
-    init: function () {
-      var login = new SW.Login({clientId: 1234});
+    onLogin: function () {
+    },
 
+    init: function () {
+      var login = new SW.Login({clientId: 'bfe9764216a6762c790e314923e277fd23064dc2c7f424c5fe930a9379f4bb71'});
+      var that = this;
       $('.download-dev-edition-button').on('click', function (e) {
         e.preventDefault();
-        login.request('login').then(function () {
-          debugger;
-          // logged in
-        });
+        login.request('login').then(that.onLogin.bind(that));
       });
     }
   };
