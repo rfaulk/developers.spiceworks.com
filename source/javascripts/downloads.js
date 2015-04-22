90e3a447388afb77f7649b5ec5d0d0a8a24882ed
David Babbitt <davidb@spiceworks.com>
2015-04-22 17:52:44 -0500

    don't open the login-to-download window anymore on IE. detects IE by inspecting styles. cross-domain cross-window postMessage doesn't work. need to figure this out differently.

--- source/javascripts/downloads.js
+++ source/javascripts/downloads.js
@@ -12,7 +12,12 @@
       var login = new SW.Login({clientId: 'bfe9764216a6762c790e314923e277fd23064dc2c7f424c5fe930a9379f4bb71'});
       $('.download-dev-edition-button').on('click', function (e) {
         e.preventDefault();
-        login.request('login').then(onLogin);
+        if ($('html').hasClass('ie')) {
+          onLogin();
+        }
+        else {
+          login.request('login').then(onLogin);
+        }
       });
     }
   };
