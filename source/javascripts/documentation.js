07c645143afa4bc99ab5bf3ab64a70cf6066870d
Michael Gerbush <michaelg@spiceworks.com>
2015-05-28 15:59:53 -0500

    Spice Button page improvements
    Fix the SpiceButton forms to post to marketo via AJAX.
    Remove inline attributes that are not supported in our current markdown 
    version.
    Manually specify the vendor javascript load order, to fix an issue where
    ladder was being included before it’s dependent library, spin.
    Copy over JS from old developers site for controlling the SpiceButton 
    forms.
    Update Ladda and Spin library versions.
    Add some basic styling for the Spice Buttons page.

--- source/javascripts/documentation.js
+++ source/javascripts/documentation.js
@@ -1,9 +1,72 @@
 (function($) {
   'use strict';
 
+  var highlightElement = function(el){
+    var $el = $(el);
+    var highlightClass = 'highlighted';
+    $el.show().addClass(highlightClass);
+    setTimeout(function(){$el.removeClass(highlightClass);},2000);
+  },
+
+  toggleLoginButtonForm = function(){
+    $('.login-with-spiceworks-form').toggle()[0].reset();
+    $('.login-button-form-thank-you').toggle();
+  },
+
+  setupLoginWithSWForm = function(){
+    var $form = $('.login-with-spiceworks-form'),
+        submitButton = Ladda.create( $form.find('.sui-bttn-primary')[0] );
+
+    $form
+      .on('submit', function(){ submitButton.start(); })
+      .on('ajax:complete', function(){
+        toggleLoginButtonForm();
+        submitButton.stop();
+        });
+  },
+
+  setFollowButtonCode = function(){
+    var vendorName = $('#vendor-name').val();
+    vendorName = vendorName || 'VENDOR_NAME';
+    $('.follow-button-code')
+      .val('<a href="http://community.spiceworks.com/pages/' + vendorName +
+        '/follow"><img src="http://static.spiceworks.com/share/follow.png"' +
+        'title="Follow us on Spiceworks" alt="Follow us on Spiceworks" /></a>');
+  },
+
+  startListeningForCopyToClipboardClicks = function(){
+    $('.copy-to-clipboard-btn')
+      .on('click', function(e){
+        e.preventDefault();
+        highlightElement($('.copy-success'));
+      })
+      .zclip({
+        path:'http://www.steamdev.com/zclip/js/ZeroClipboard.swf',
+        copy: function(){return $('#' + $(this).data('clipboard-source')).val();},
+        afterCopy: function(){highlightElement($(this).data('clipboard-source'));}
+      });
+  };
+
   developers.documentation = {
     share: {
       init: function () {
+        setupLoginWithSWForm();
+
+        //set up events for buttons
+        $('.update-follow-button-code').on('click', function(e){
+          e.preventDefault();
+          setFollowButtonCode();
+          highlightElement(this);
+        });
+
+        $('.show-login-button-form').on('click', function (e) {
+          e.preventDefault();
+          toggleLoginButtonForm();
+        });
+
+        //set up zClip copy-to-clipboard functionality
+        startListeningForCopyToClipboardClicks();
+
         var $sideBar = $('.aside-wrapper aside');
 
         $sideBar.affix({
